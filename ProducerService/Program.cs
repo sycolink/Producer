5c899f8129cb3732363d91e3fbd6aba134111d57
Lunch <vassiliou.thomas@gmail.com>
2017-09-09 14:48:16 +0100

    Basic skeleton service with logging

---
+++ ProducerService/Program.cs
@@ -0,0 +1,29 @@
+﻿using Topshelf;
+using log4net.Config;
+
+namespace ProducerService
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            HostFactory.Run(x =>                                 //1
+            {
+                XmlConfigurator.Configure();
+                x.UseLog4Net();
+                x.Service<ServiceHost>(s =>                        //2
+                {
+                    s.ConstructUsing(name => new ServiceHost());     //3
+                    s.WhenStarted(tc => tc.Start());              //4
+                    s.WhenStopped(tc => tc.Stop());
+                    //5
+                });
+                x.RunAsLocalSystem();                            //6
+
+                x.SetDescription("A simple service to play around with RabbitMQ Producers");        //7
+                x.SetDisplayName("My RabbitMQ Producer");                       //8
+                x.SetServiceName("MyRabbitMQProducer");                       //9
+            });                                                  //10
+        }
+    }
+}
---
+++ ProducerService/ServiceHost.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using System.Reflection;
+
+namespace ProducerService
+{
+    class ServiceHost
+    {
+        private readonly ILog _log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ServiceHost()
+        {
+        }
+
+        public void Start()
+        {
+        }
+
+        public void Stop()
+        {
+        }
+    }
+}
